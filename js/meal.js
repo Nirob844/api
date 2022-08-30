d2f05d1f7bcebe21ff14303a248b8b7690d66140
Nirob Hasan <nirob427417@gmail.com>
2022-08-30 16:31:53 +0600

    done

--- js/meal.js
+++ js/meal.js
@@ -31,3 +31,31 @@ const searchFood = () => {
     loadMeals(searchText);
     searchField.value = '';
 }
+const loadMealDetail = (idMeal) => {
+    // console.log('get details of id', idMeal);
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+
+    //  console.log(url);
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetails(data.meals[0]));
+}
+const displayMealDetails = meal => {
+    const detailContainer = document.getElementById('detail-container');
+    detailContainer.innerHTML = ``;
+    const mealDiv = document.createElement('div');
+    mealDiv.classList.add('card');
+    mealDiv.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">${meal.strInstructions}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    `;
+    detailContainer.appendChild(mealDiv);
+}
+
+
+loadMeals('');
